d5b31e9ea11c2a6e69c08b1db0a24882178f9af2
Dmitry Podrez <dima.padrez.00@icloud.com>
2024-09-19 07:28:01 +0200

    new file:   hangman-code.py   init: create a list with words

---
+++ hangman-code.py
@@ -0,0 +1,39 @@
+import random
+word_list = [
+    # Animals
+    "alligator", "antelope", "baboon", "butterfly", "cheetah", "crocodile", "dolphin", 
+    "elephant", "flamingo", "giraffe", "kangaroo", "octopus", "rhinoceros", "squirrel", "tarantula",
+    
+    # Countries
+    "Australia", "Argentina", "Belgium", "Brazil", "Cambodia", "Denmark", "Ethiopia", 
+    "Finland", "Germany", "Honduras", "Indonesia", "Jamaica", "Kazakhstan", "Luxembourg", "Madagascar",
+    
+    # Professions
+    "architect", "astronomer", "biologist", "carpenter", "detective", "engineer", 
+    "firefighter", "geologist", "journalist", "lawyer", "musician", "pharmacist", "pilot", 
+    "psychologist", "veterinarian",
+    
+    # Colors
+    "aquamarine", "burgundy", "chartreuse", "cyan", "emerald", "fuchsia", "indigo", 
+    "lavender", "magenta", "maroon", "navy", "olive", "periwinkle", "turquoise", "violet",
+    
+    # Food
+    "avocado", "broccoli", "cappuccino", "chocolate", "croissant", "doughnut", "espresso", 
+    "grapefruit", "lasagna", "mozzarella", "pancake", "quesadilla", "ravioli", "strawberry", "zucchini",
+    
+    # Technology
+    "algorithm", "bandwidth", "calculator", "database", "encryption", "firewall", 
+    "keyboard", "microchip", "network", "processor", "quantum", "robotics", "software", 
+    "transistor", "wireless",
+    
+    # Nature
+    "avalanche", "canyon", "desert", "earthquake", "glacier", "hurricane", "jungle", 
+    "lightning", "monsoon", "ocean", "rainforest", "tornado", "volcano", "waterfall", "wildfire",
+    
+    # Space
+    "asteroid", "blackhole", "comet", "galaxy", "meteorite", "nebula", "orbit", 
+    "planet", "satellite", "spacecraft", "stardust", "supernova", "telescope", "universe", "wormhole",
+    
+    # Random Hard Words
+    "bureaucracy", "cacophony", "dichotomy", "exacerbate", "facetious", "juxtaposition"
+]
